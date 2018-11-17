005c14c2c2ddf5fab4933f10c5f997aede112339
GupiSakaguchi <gupisakaguchi@gmail.com>
2018-11-18 08:36:10 +0900

    sepalate book versions

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module VersionSandbox
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.autoload_paths << "#{Rails.root}/app/histories"
   end
 end
